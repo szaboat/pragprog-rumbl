8907fd053a9362568e6b1140586fec783d32c6ce
Attila Szabo <attila.szabo@gmail.com>
2019-08-02 13:45:05 +0200

    Craete Auth plug

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -4,6 +4,8 @@ defmodule RumblWeb.UserController do
   alias Rumbl.Accounts
   alias Rumbl.Accounts.User
 
+  plug :authenticate when action in [:index, :show, :new]
+
   def show(conn, %{"id" => id}) do
     user = Accounts.get_user(id)
     render(conn, "show.html", user: user)
@@ -28,6 +30,17 @@ defmodule RumblWeb.UserController do
 
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  defp authenticate(conn, _options) do
+    if (conn.assigns.current_user) do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page!")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
     end
   end
 end
