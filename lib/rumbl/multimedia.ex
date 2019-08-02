f6a861304d1bdb78a991d5c2ba5f50900f33ad49
Attila Szabo <attila.szabo@gmail.com>
2019-08-02 17:46:37 +0200

    Handle user - video relation in controller

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -4,9 +4,10 @@ defmodule Rumbl.Multimedia do
   """
 
   import Ecto.Query, warn: false
-  alias Rumbl.Repo
 
+  alias Rumbl.Accounts
   alias Rumbl.Multimedia.Video
+  alias Rumbl.Repo
 
   @doc """
   Returns the list of videos.
@@ -49,9 +50,10 @@ defmodule Rumbl.Multimedia do
       {:error, %Ecto.Changeset{}}
 
   """
-  def create_video(attrs \\ %{}) do
+  def create_video(%Accounts.User{} = user, attrs \\ %{}) do
     %Video{}
     |> Video.changeset(attrs)
+    |> put_user(user)
     |> Repo.insert()
   end
 
@@ -98,7 +100,29 @@ defmodule Rumbl.Multimedia do
       %Ecto.Changeset{source: %Video{}}
 
   """
-  def change_video(%Video{} = video) do
-    Video.changeset(video, %{})
+  def change_video(%Accounts.User{} = user, %Video{} = video) do
+    video
+    |> Video.changeset(%{})
+    |> put_user(user)
+  end
+
+  defp put_user(changeset, user) do
+    Ecto.Changeset.put_assoc(changeset, :user, user)
+  end
+
+  def list_user_videos(%Accounts.User{} = user) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.all()
+  end
+
+  def get_user_video!(%Accounts.User{} = user, id) do
+    from(v in Video, where: v.id == ^id)
+    |> user_videos_query(user)
+    |> Repo.one!()
+  end
+
+  defp user_videos_query(query, %Accounts.User{id: user_id}) do
+    from(v in query, where: v.user_id == ^user_id)
   end
 end
