776054751f8db8329256cfcb355c4419e81aac2f
Attila Szabo <attila.szabo@gmail.com>
2019-08-05 10:35:14 +0200

    Create categories

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -7,6 +7,7 @@ defmodule Rumbl.Multimedia do
 
   alias Rumbl.Accounts
   alias Rumbl.Multimedia.Video
+  alias Rumbl.Multimedia.Category
   alias Rumbl.Repo
 
   @doc """
@@ -132,5 +133,9 @@ defmodule Rumbl.Multimedia do
 
   defp preload_user(video_or_videos) do
     Repo.preload(video_or_videos, :user)
+  end
+
+  def create_category(name) do
+    Repo.get_by(Category, name: name) || Repo.insert!(%Category{name: name})
   end
 end
