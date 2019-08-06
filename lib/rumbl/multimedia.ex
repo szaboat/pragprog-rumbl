dea581eca7c644d48f06b32cff5f976b664e663a
Attila Szabo <attila.szabo@gmail.com>
2019-08-06 16:36:05 +0200

    Channels: persist annotations

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -9,6 +9,8 @@ defmodule Rumbl.Multimedia do
   alias Rumbl.Multimedia.Video
   alias Rumbl.Multimedia.Category
   alias Rumbl.Repo
+  alias Rumbl.Multimedia.Annotation
+
 
   @doc """
   Returns the list of videos.
@@ -143,5 +145,21 @@ defmodule Rumbl.Multimedia do
     Category
     |> Category.alphabetical()
     |> Repo.all()
+  end
+
+  def annotate_video(%Accounts.User{} = user, video_id, attrs) do
+    %Annotation{video_id: video_id}
+    |> Annotation.changeset(attrs)
+    |> put_user(user)
+    |> Repo.insert()
+  end
+
+  def list_annotations(%Video{} = video) do
+    Repo.all(
+      from a in Ecto.assoc(video, :annotations),
+        order_by: [asc: a.at, asc: a.id],
+        limit: 500,
+        preload: [:user]
+    )
   end
 end
