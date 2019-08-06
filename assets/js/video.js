901e0d3cb70f6aeb6c0d53a088d3bf9b4b0b7406
Attila Szabo <attila.szabo@gmail.com>
2019-08-06 15:19:24 +0200

    Ping with channels

--- assets/js/video.js
+++ assets/js/video.js
@@ -13,7 +13,6 @@ let Video = {
     Player.init(element.id, playerId, () => {
       this.onReady(videoId, socket);
     });
-    
   },
 
   onReady(videoId, socket) {
@@ -21,6 +20,13 @@ let Video = {
     let msgInput = document.getElementById("msg-input");
     let postButton = document.getElementById("msg-submit");
     let vidChannel = socket.channel("videos:" + videoId);
+
+    vidChannel.on("ping", ({ count }) => console.log("PING", count));
+
+    vidChannel
+      .join()
+      .receive("ok", resp => console.log("joined the video channel", resp))
+      .receive("error", reason => console.log("join failed", reason));
   }
 };
 
